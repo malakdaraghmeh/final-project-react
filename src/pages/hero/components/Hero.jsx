20ebad4ece848b39fa7d9abb56e28f035a508e13
malakdaraghmeh <malakdaraghmeh23@gmail.com>
2024-04-09 01:20:49 +0300

    finalproject

--- src/pages/hero/components/Hero.jsx
+++ src/pages/hero/components/Hero.jsx
@@ -4,7 +4,7 @@ import "./hero.css";
 export default function Hero() {
   return (
     <>
-      <div className="container w-100">
+      <div className="container w-100 ">
         <img className="img_hero" src="hero.jpg" />
       </div>
     </>
